1b81fe49805a4aeb7314f1de9fa5152a983fd833
Steve Milburn <scmilburn@gmail.com>
2016-09-30 13:41:47 -0400

    webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ var webpack = require('webpack');
 var WebpackNotifierPlugin = require('webpack-notifier');
 
 var PATHS = {
-  src: path.join(__dirname, 'public/src/js/main.js'),
-  build: path.join(__dirname, 'public/dist/')
+  src: path.join(__dirname, 'src/js/'),
+  build: path.join(__dirname, 'app/public/build/')
 };
 
 module.exports = {
@@ -16,35 +16,44 @@ module.exports = {
     filename: '[name].js'
   },
   module: {
-    preLoaders: [
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, '/public/src/js')
-        ],
-        loader: 'eslint'
-      }
-    ],
     loaders: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react', 'stage-1']
         }
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.css/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\S+)?$/,
+        loader: 'file-loader'
       }
     ]
   },
-  eslint: {
-    failOnWarning: false,
-    failOnError: true
-  },
   plugins: [
     new webpack.ProvidePlugin({
-      'Promise': 'es6-promise',
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+      'Promise': 'es6-promise'
     }),
-    new WebpackNotifierPlugin()
+    new WebpackNotifierPlugin(),
+    // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
+    // inside your code for any environment checks; UglifyJS will automatically
+    // drop any unreachable code.
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
   ]
 };
